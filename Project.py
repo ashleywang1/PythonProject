d596435e7a736b32c83230e94791b1d9f3a75c33
Ashley Wang <ashwang@mit.edu>
2014-01-28 13:48:05 -0500

    items and person combined

--- Project.py
+++ Project.py
@@ -11,23 +11,28 @@ def new_game(size):
 
     window_size = [size, size]
     screen = pygame.display.set_mode(window_size)
-    pygame.display.set_caption("Items")
+    pygame.display.set_caption("Items and Person")
+    
+    board = Board(screen.get_size())
+    main_loop(screen, board)
 
-    main_loop(screen)
-
-def main_loop(screen):
+def main_loop(screen, board):
     background = pygame.Surface(screen.get_size())
     background.fill(white)
     screen.blit(background, (0, 0))
+    
+    board.theHero.draw(screen)
     pygame.display.flip()
-
-    board = Board(screen.get_size())
     
     items = board.items
 
     s = 0
 
-    while s < 80:
+    while s < 180:
+        #Erase previous images
+        screen.blit(background, (0, 0))
+
+        #Draw the items
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -38,7 +43,30 @@ def main_loop(screen):
             i.move()
             screen.blit(i.image, i.pos)
 
+
+        #Draw the person
+        board.theHero.draw(screen)
+
+        #Sense mouse position
+        mouse = pygame.mouse.get_pos()
+        x = mouse[0]
+        y = mouse[1]
+
+        hero = board.person
+        heroPosition = hero.rect
+
+        if x > heroPosition.x+25:
+            #"person moving to the right"
+            hero.move_right(x, board.width)
+            
+        elif x < heroPosition.x+25:
+            #board.person.move_left(x)
+            hero.move_left(x, board.width)
+        else:
+            print "person stopped"
+        
         pygame.display.update()
+        pygame.display.flip()
         pygame.time.delay(100)
         s += 1
     pygame.quit()
@@ -49,13 +77,15 @@ class Board:
     def __init__(self, size):
 
         self.size = size
+        self.width = size[0]
+        self.height = size[1]
 
         self.items = pygame.sprite.RenderPlain()
 
-        """self.player = Player(self)
-                          
-        self.thePlayer = pygame.sprite.RenderPlain()
-        self.thePlayer.add(self.player)"""
+        personSize = 50
+        self.person = Person(self, self.width/2, self.height-personSize, 50)
+        self.theHero = pygame.sprite.RenderPlain()
+        self.theHero.add(self.person)
 
     def makeItems(self):
             i = Item(random.choice(types), random.randint(0, self.size[0] - 20), 5)
@@ -78,6 +108,45 @@ class Item(pygame.sprite.Sprite):
         self.pos = self.pos.move(0, self.speed)
         if self.pos.top <= 400:
             return False
+
+class Person (pygame.sprite.Sprite):
+    def __init__(self, board, col, row, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.col = col
+        self.row = row
+        self.board = board
+        #this needs to be changed
+        self.rect = pygame.Rect(col, row, size, size)
+        print col, row
+        
+        self.image = self.image = pygame.image.load("girlCharacter.png").convert()
+
+
+    def move_right(self, destination, size):
+        """Move the person to the right"""
+        WIDTH = 50
+        HEIGHT = 50
+        if destination > WIDTH*size-WIDTH/2:
+            velocity =0
+        else:
+            velocity = (destination - self.rect.x-WIDTH/2)/3
+            
+        x = self.rect.x + velocity
+        y = self.rect.y
+        self.rect = pygame.Rect(x, y, WIDTH, HEIGHT)
+        
+    def move_left(self, destination, size):
+        """Move the person to the right"""
+        WIDTH = 50
+        HEIGHT = 50
+        if destination < WIDTH/2:
+            velocity = 0
+        else:
+            velocity = (self.rect.x+WIDTH/2 - destination)/3
+        x = self.rect.x - velocity
+        y = self.rect.y
+        self.rect = pygame.Rect(x, y, 50, 50)
+ 
 
 if __name__ == "__main__":
     new_game(400)
